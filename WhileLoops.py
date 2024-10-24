0fbbe06740b01c75be1ba2413fba064403215de1
GeorgeGarnett-O <gg403@kent.ac.uk>
2024-10-24 13:10:37 +0000

    while loops number 1,100

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,7 +1,6 @@
 ### Part Two -- your code goes here. 
 import random 
 
-number_variable = input(random.randint(1, 100))
 number_variable = random.randint(1, 100)
 
 determinant = int(input("the corect number from the number variable randomizer"))
@@ -9,18 +8,13 @@ loop = True
 
 while loop == True:
 
-if determinant > number_variable :
+  if determinant !=  number_variable :
     determinant = int(input("number from the number variable randomizer"))
     if determinant > number_variable :
         print  ("too high a number")
     elif determinant < number_variable : 
         print  ("too low of a number")
-    elif determiant == numbr_variable: 
+    elif determinant == number_variable: 
         print  ( "correct number") 
+        
         loop = False 
-
-   print  ("too high a number")
-elif determinant < number_variable : 
-   print  ("too low a number")
-elif determinant == number_variable: 
-   print  ( "correct number")
