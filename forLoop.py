030f2cf6a9f51b795c356519c7e58861d1ac8981
GeorgeGarnett-O <gg403@kent.ac.uk>
2024-10-24 13:30:31 +0000

    for loop

--- forLoop.py
+++ forLoop.py
@@ -1,1 +1,10 @@
 ### Part One -- your code goes here. 
+EVEN = int(input(1,2,3,4,5,6,7,8,9,10))
+EVEN_C = ( "only give even numbers found in even")
+
+if EVEN > 10 :
+    print ("odd")
+    else ODD < 10:
+        print ("even")
+
+
