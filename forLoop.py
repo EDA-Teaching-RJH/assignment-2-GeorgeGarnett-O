484e2f127d281783931860b4f41fbfa9f46ef205
GeorgeGarnett-O <gg403@kent.ac.uk>
2024-10-25 14:23:18 +0000

    for loop

--- forLoop.py
+++ forLoop.py
@@ -1,36 +1,40 @@
 ### Part One -- your code goes here. 
+
 parity=int (input ("an number bewtween 0 and 10"))
 
-print ("the number is ")
+print ("the squared number is  ")
 
-if parity >= 10: 
-  print ("even")
-elif parity >= 9 :
-    print ("odd")
+for _ in range(1):
 
-elif parity >= 8: 
-  print ("even")
+  if parity >= 10: 
+    print ("100")
 
-elif parity >= 7 :
-  print ("odd")
+  elif parity >= 9 :
+    print ("81")
 
-elif parity >= 6:
-  print ("even")
+  elif parity >= 8: 
+    print ("64")
 
-elif parity >=5 :
-  print ("odd")
+  elif parity >= 7 :
+    print ("49")
 
-elif parity >= 4:
-  print ("even")
+  elif parity >= 6:
+    print ("36")
 
-elif parity >=3:
-  print ("odd")
+  elif parity >=5 :
+    print ("25")
 
-elif parity >= 2:
-  print ("even")
+  elif parity >= 4:
+    print ("16")
 
-elif parity >=1:
-  print ("odd")
+  elif parity >=3:
+    print ("9")
+
+  elif parity >= 2:
+    print ("4")
+
+  elif parity >=1:
+    print ("2")
 
 
 
