11b9b0c090dcf2021f23cbcc084361fc90b365d1
GeorgeGarnett-O <gg403@kent.ac.uk>
2024-10-27 12:41:50 +0000

    list mulipulation

--- ListManipulation.py
+++ ListManipulation.py
@@ -1,1 +1,8 @@
-### Part Three -- your code goes here. 
+
+list_mulipulation = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+list_mulipulation.sort()
+
+list_mulipulation.extend([7, 8])
+
+print(list_mulipulation)
